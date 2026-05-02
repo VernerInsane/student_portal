21c1a57229e55e1e08c816c4e93cc0aad5e61776
VernerInsane <sasha.seva.1996@gmail.com>
2026-05-02 15:16:51 +0800

    Changed version

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 """Simple Student Portal Application"""
 
-
 def main():
-    print("Student Portal System")
+    print("Hi,this is Student Portal System. Glad to see you!")
 
+def login(username):
+    print(f"User {username} successfully logged in.")
 
 if __name__ == "__main__":
     main()
