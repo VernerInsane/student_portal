310934e93629a7751cd224d506b299acb98ab475
VernerInsane <sasha.seva.1996@gmail.com>
2026-05-02 15:28:05 +0800

    Delete part of main message

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Simple Student Portal Application"""
 
 def main():
-    print("Hi,this is Student Portal System. Glad to see you!")
+    print("Hi,this is Student Portal System.")
 
 def login(username):
     print(f"User {username} successfully logged in. Please log in now.")
