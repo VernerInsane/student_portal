d4ae0eba79fda7bdff03a0f8eec937bc0ff5d96e
VernerInsane <sasha.seva.1996@gmail.com>
2026-05-02 15:22:56 +0800

    Changed login message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def main():
     print("Hi,this is Student Portal System. Glad to see you!")
 
 def login(username):
-    print(f"User {username} successfully logged in.")
+    print(f"User {username} successfully logged in. Please log in now.")
 
 if __name__ == "__main__":
     main()
