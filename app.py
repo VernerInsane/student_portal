124e4995af9648ffd3e59d6e82f07db68369540c
VernerInsane <sasha.seva.1996@gmail.com>
2026-05-02 15:18:57 +0800

    Added logout function

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ def main():
 def login(username):
     print(f"User {username} successfully logged in.")
 
+def logout(username):
+    print(f"User {username} logged out.")
+
 if __name__ == "__main__":
     main()
